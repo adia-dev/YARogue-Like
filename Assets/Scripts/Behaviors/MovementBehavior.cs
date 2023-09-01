a17f720488d2ffe3b2dd66f69acf06b54e085d9b
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-09-01 21:07:31 +0200

    Turn in place 180 or Turn + Walk 180
    - Added support for controller

--- Assets/Scripts/Behaviors/MovementBehavior.cs
+++ Assets/Scripts/Behaviors/MovementBehavior.cs
@@ -1,12 +1,9 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementBehavior : MonoBehaviour
 {
-
-
     [Header("Movement Parameters")]
     public float walkSpeed = 3.0f;
     public float runSpeed = 6.0f;
@@ -22,144 +19,190 @@ public class MovementBehavior : MonoBehaviour
     public float gravity = -9.81f;
     public LayerMask groundLayer;
 
-
     [Header("Collision Parameters")]
+    [Range(0.05f, 1.0f)]
     public float groundCheckRadius = 0.1f;
 
     [Header("Debug")]
     public Color groundCheckColor = Color.magenta;
-
+    public bool drawGroundCheck = false;
 
     private CharacterController characterController;
     private Animator animator;
-
     private Vector3 moveDirection = Vector3.zero;
+    private Vector3 directionInput;
 
-    float speed;
-    float speedSmoothVelocity;
-    float turnSmoothVelocity;
+    private float speed;
+    private float speedSmoothVelocity;
+    private float turnSmoothVelocity;
+    private bool hasDoubledJumped = false;
+    private bool isGrounded = false;
 
-    bool hasDoubledJumped = false;
-    bool isGrounded = false;
-
-    int lastGroundedFrame = 0;
+    private int lastGroundedFrame = 0;
 
     // Animator Hashes
-    int moveInputMagHash = Animator.StringToHash("MoveInputMag");
-    int moveInputXHash = Animator.StringToHash("MoveInputX");
-    int moveInputYHash = Animator.StringToHash("MoveInputY");
-    int isRunningHash = Animator.StringToHash("IsRunning");
-    int isCrouchingHash = Animator.StringToHash("IsCrouching");
-    int isGroundedHash = Animator.StringToHash("IsGrounded");
-    int speedHash = Animator.StringToHash("Speed");
-    int verticalSpeedHash = Animator.StringToHash("VerticalSpeed");
-    int doubleJumpedHash = Animator.StringToHash("DoubleJumped");
-    int JumpHash = Animator.StringToHash("Jump");
+    private readonly int moveInputMagHash = Animator.StringToHash("MoveInputMag");
+    private readonly int moveInputXHash = Animator.StringToHash("MoveInputX");
+    private readonly int moveInputYHash = Animator.StringToHash("MoveInputY");
+    private readonly int isRunningHash = Animator.StringToHash("IsRunning");
+    private readonly int isCrouchingHash = Animator.StringToHash("IsCrouching");
+    private readonly int isGroundedHash = Animator.StringToHash("IsGrounded");
+    private readonly int speedHash = Animator.StringToHash("Speed");
+    private readonly int verticalSpeedHash = Animator.StringToHash("VerticalSpeed");
+    private readonly int doubleJumpedHash = Animator.StringToHash("DoubleJumped");
+    private readonly int JumpHash = Animator.StringToHash("Jump");
+    private readonly int MoveInputDot = Animator.StringToHash("MoveInputDot");
 
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        // Reserved for future use.
     }
 
     public void HandleMovement()
     {
-        InputManager inputManagerInstance = InputManager.Instance;
+        PerformGroundCheck();
+        HandleDirection();
+        HandleSpeedAndMovement();
+        HandleJumping();
+        HandleAnimations();
+    }
 
-        // Ground check
+    private void PerformGroundCheck()
+    {
         isGrounded = Physics.CheckSphere(transform.position, groundCheckRadius, groundLayer);
-        animator.SetBool(isGroundedHash, isGrounded);
+    }
 
-        // Get Input
-        Vector2 moveInput = inputManagerInstance.MoveInput;
-        Vector3 direction = new(moveInput.x, 0f, moveInput.y);
 
-        // Handle movement direction based on camera
-        Vector3 move = (cameraTransform.forward * direction.z + cameraTransform.right * direction.x) * (inputManagerInstance.MovementLocked ? 0 : 1);
-        move.y = 0f;
+    private void HandleDirection()
+    {
+        Vector2 moveInput = InputManager.Instance.MoveInput;
+        directionInput = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
 
-        if (moveInput.magnitude != 0 && !inputManagerInstance.RotationLocked)
+        if (moveInput.magnitude != 0 && !InputManager.Instance.RotationLocked)
         {
-            float targetRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float targetRotation = Mathf.Atan2(directionInput.x, directionInput.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
         }
+    }
 
 
-        // Handle speed
-        float targetSpeed = walkSpeed;
-        if (inputManagerInstance.ShouldRun)
+    private void HandleSpeedAndMovement()
+    {
+        Vector2 moveInput = InputManager.Instance.MoveInput;
+        float targetSpeed = isGrounded && InputManager.Instance.ShouldCrouch ? crouchSpeed : InputManager.Instance.ShouldRun ? runSpeed : walkSpeed;
+        speed = Mathf.SmoothDamp(speed, targetSpeed * moveInput.magnitude, ref speedSmoothVelocity, speedSmoothTime);
+        characterController.Move(speed * Time.deltaTime * transform.forward);
+    }
+
+    private void HandleJumping()
+    {
+        if ((isGrounded || !hasDoubledJumped) && InputManager.Instance.ShouldJump)
         {
-            targetSpeed = runSpeed;
-        }
+            if (!isGrounded)
+            {
+                hasDoubledJumped = true;
+                animator.SetBool(doubleJumpedHash, true);
+            }
+            else
+            {
+                animator.SetTrigger(JumpHash);
+            }
 
-        if (isGrounded && inputManagerInstance.ShouldCrouch)
-        {
-            targetSpeed = crouchSpeed;
-        }
-
-        if (isGrounded)
-        {
-            hasDoubledJumped = false;
-            animator.SetBool(doubleJumpedHash, false);
-            animator.SetBool(JumpHash, false);
-
-            if(lastGroundedFrame > 1)
-                moveDirection.y = 0.0f;
-
-            lastGroundedFrame++;
-        }else {
+            moveDirection.y = jumpForce;
+            InputManager.Instance.ShouldJump = false;
             lastGroundedFrame = 0;
         }
+        else if (isGrounded)
+        {
+            if (lastGroundedFrame > 1)
+            {
+                moveDirection.y = 0.0f;
+            }
 
-        speed = Mathf.SmoothDamp(speed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
 
-        // Move the character
-        characterController.Move(speed * Time.deltaTime * move);
+            animator.ResetTrigger(JumpHash);
+            animator.SetBool(doubleJumpedHash, false);
 
-        // Handle Animations
+            lastGroundedFrame++;
+        }
+
+        moveDirection.y += gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void HandleAnimations()
+    {
+        Vector2 moveInput = InputManager.Instance.MoveInput;
+
         animator.SetFloat(speedHash, speed);
         animator.SetFloat(verticalSpeedHash, moveDirection.y);
         animator.SetFloat(moveInputMagHash, moveInput.magnitude);
         animator.SetFloat(moveInputXHash, moveInput.x);
         animator.SetFloat(moveInputYHash, moveInput.y);
-        animator.SetBool(isRunningHash, inputManagerInstance.ShouldRun);
-        animator.SetBool(isCrouchingHash, inputManagerInstance.ShouldCrouch);
+        animator.SetBool(isRunningHash, InputManager.Instance.ShouldRun);
+        animator.SetBool(isCrouchingHash, InputManager.Instance.ShouldCrouch);
+        animator.SetBool(isGroundedHash, isGrounded);
+        animator.SetFloat(MoveInputDot, GetMoveInputDot());
+    }
 
-        // Handle Jumping
-        if ((isGrounded || !hasDoubledJumped) && inputManagerInstance.ShouldJump)
-        {
-        
-            animator.SetBool(JumpHash, true);
+    private float GetMoveInputDot()
+    {
+        Vector3 forward = transform.forward;
+        Vector2 moveInput = InputManager.Instance.MoveInput;
 
-            if (!isGrounded) { 
-                hasDoubledJumped = true;
-                animator.SetBool(doubleJumpedHash, true);
-                moveDirection.y = jumpForce;
-            } else { 
-                moveDirection.y = jumpForce;
-            }
+        Vector3 cameraDirectionInput = cameraTransform.TransformDirection(directionInput);
+        cameraDirectionInput.y = 0.0f;
+        cameraDirectionInput.Normalize();
 
-            lastGroundedFrame = 0;
-            inputManagerInstance.ShouldJump = false;
-        }
+        return Vector3.Dot(forward, cameraDirectionInput);
 
-        // Apply gravity
-        moveDirection.y += gravity * Time.deltaTime;
-
-        // Final Move
-        characterController.Move(moveDirection * Time.deltaTime);
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = isGrounded ? groundCheckColor : Color.red;
-        Gizmos.DrawWireSphere(transform.position, groundCheckRadius);
+
+        if (characterController != null && drawGroundCheck)
+        {
+            Gizmos.DrawWireSphere(transform.position, groundCheckRadius);
+
+            for (float i = -characterController.radius; i < characterController.radius; i += groundCheckRadius)
+            {
+                Gizmos.DrawLine(transform.position - Vector3.right * i, transform.position - Vector3.right * i + Vector3.down * groundCheckRadius);
+            }
+        }
+
+        // Combine the player's rotation with the camera's rotation
+        float cameraAngle = transform.eulerAngles.y + cameraTransform.eulerAngles.y;
+
+        // Convert the angle to a vector
+        Vector3 cameraForward = cameraTransform.forward;
+        cameraForward.y = 0;
+        cameraForward.Normalize();
+
+        // Draw a line from the player in the direction the camera is facing
+        Gizmos.color = Color.blue;  // Change color for this specific line, if you want
+        Gizmos.DrawLine(transform.position + Vector3.up, transform.position + Vector3.up + cameraForward * 2.0f);
+        Gizmos.DrawWireSphere(transform.position + Vector3.up + cameraForward * 2.0f, groundCheckRadius);
+
+        Gizmos.color = Color.magenta;  // Change color for this specific line, if you want
+        Gizmos.DrawLine(transform.position + Vector3.up, transform.position + Vector3.up + transform.forward * 2.0f);
+        Gizmos.DrawWireSphere(transform.position + Vector3.up + transform.forward * InputManager.Instance.MoveInput.magnitude * 2.0f, groundCheckRadius);
+
+
+        Vector3 cameraDirectionInput = cameraTransform.TransformDirection(directionInput);
+        cameraDirectionInput.y = 0.0f;
+        cameraDirectionInput.Normalize();
+
+        Gizmos.color = Color.red;  // Change color for this specific line, if you want
+        Gizmos.DrawLine(transform.position + Vector3.up, transform.position + Vector3.up + cameraDirectionInput * directionInput.magnitude * 2.0f);
+        Gizmos.DrawWireSphere(transform.position + Vector3.up + cameraDirectionInput * directionInput.magnitude * 2.0f, groundCheckRadius);
     }
 }
+
